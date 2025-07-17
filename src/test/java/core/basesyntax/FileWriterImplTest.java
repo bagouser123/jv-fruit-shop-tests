81314d88fa5f43428224a13c906442cc36a58fb7
Ілля Лазарев <108271589+bagouser123@users.noreply.github.com>
2025-07-18 01:14:54 +0300

    making tests for fruit shop

---
+++ src/test/java/core/basesyntax/FileWriterImplTest.java
@@ -0,0 +1,16 @@
+package core.basesyntax;
+
+import core.basesyntax.service.FileWriterImpl;
+import core.basesyntax.service.Writer;
+import org.junit.jupiter.api.Test;
+
+public class FileWriterImplTest {
+    private static final String FILE_TO = "src/main/resources/ValueToFileDataBase.csv";
+    private Writer writer = new FileWriterImpl();
+
+    @Test
+    void checkFileWriter_Ok() {
+        String report = "banana,100";
+        writer.write(report, FILE_TO);
+    }
+}
---
+++ src/test/java/core/basesyntax/ShopServiceImplTest.java
@@ -0,0 +1,39 @@
+package core.basesyntax;
+
+import core.basesyntax.model.FruitTransaction;
+import core.basesyntax.service.ShopService;
+import core.basesyntax.service.ShopServiceImpl;
+import core.basesyntax.service.operation.BalanceOperation;
+import core.basesyntax.service.operation.OperationHandler;
+import core.basesyntax.service.operation.PurchaseOperation;
+import core.basesyntax.service.operation.ReturnOperation;
+import core.basesyntax.service.operation.SupplyOperation;
+import core.basesyntax.strategy.OperationStrategy;
+import core.basesyntax.strategy.OperationStrategyImpl;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+
+public class ShopServiceImplTest {
+
+    @Test
+    void shopServiceTest_Ok() {
+        List<FruitTransaction> transactions = new ArrayList<>();
+        FruitTransaction.Operation operation = FruitTransaction.Operation.SUPPLY;
+        String fruit = "apple";
+        int amount = 100;
+
+        FruitTransaction transaction = new FruitTransaction(operation, fruit, amount);
+        transactions.add(transaction);
+        Map<FruitTransaction.Operation, OperationHandler> operationHandlerMap = new HashMap<>();
+        operationHandlerMap.put(FruitTransaction.Operation.BALANCE, new BalanceOperation());
+        operationHandlerMap.put(FruitTransaction.Operation.PURCHASE, new PurchaseOperation());
+        operationHandlerMap.put(FruitTransaction.Operation.RETURN, new ReturnOperation());
+        operationHandlerMap.put(FruitTransaction.Operation.SUPPLY, new SupplyOperation());
+        OperationStrategy operationStrategy = new OperationStrategyImpl(operationHandlerMap);
+        ShopService shopService = new ShopServiceImpl(operationStrategy);
+        shopService.process(transactions);
+    }
+}
