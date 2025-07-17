46b86832716d694f26d2d50e30a02512d9dd03ac
Ілля Лазарев <108271589+bagouser123@users.noreply.github.com>
2025-07-17 18:35:28 +0300

    making tests for fruit shop

--- src/main/java/core/basesyntax/dao/FruitShopDao.java
+++ src/main/java/core/basesyntax/dao/FruitShopDao.java
@@ -1,9 +1,5 @@
 package core.basesyntax.dao;
 
-import core.basesyntax.model.FruitTransaction;
-
 public interface FruitShopDao {
     void add(String fruit, int amount);
-
-    FruitTransaction get(String fruit);
 }
