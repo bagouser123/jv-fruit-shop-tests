117573f6a0b877b4997b2d36dfd3182cb42542a9
Ілля Лазарев <108271589+bagouser123@users.noreply.github.com>
2025-07-17 18:40:22 +0300

    making tests for fruit shop

--- src/main/java/core/basesyntax/db/StorageForTests.java
+++
@@ -1,9 +0,0 @@
-package core.basesyntax.db;
-
-import core.basesyntax.model.FruitTransaction;
-import java.util.ArrayList;
-import java.util.List;
-
-public class StorageForTests {
-    public static final List<FruitTransaction> storage = new ArrayList<>();
-}
